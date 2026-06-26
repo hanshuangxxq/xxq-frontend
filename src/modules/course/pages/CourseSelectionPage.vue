75496df3d07b8edab48535c2f123a2d85af1a961
寒霜 <gangziwo620@163.com>
2026-06-26 12:59:39 +0800

    feat: relocate language switcher to sidebar and add course module wiring
    - Move NSelect locale switcher from App.vue fixed-position overlay to
      sidebar menu in MainLayout.vue
    - Wire up course module route (/course) and i18n namespaces
    - Add select.svg icon for sidebar language switcher entry

---
+++ src/modules/course/pages/CourseSelectionPage.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+import { useI18n } from 'vue-i18n'
+import { NCard, NSpace, NEmpty } from 'naive-ui'
+
+const { t } = useI18n()
+</script>
+
+<template>
+  <div style="padding: 24px">
+    <NSpace vertical :size="16">
+      <NCard :title="$t('course.title')">
+        <NEmpty :description="$t('course.noCourses')" />
+      </NCard>
+    </NSpace>
+  </div>
+</template>
