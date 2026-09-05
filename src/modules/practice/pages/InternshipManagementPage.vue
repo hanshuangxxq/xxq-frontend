647f053728704bcec94c8c3712ff54a79621b9d4
寒霜 <gangziwo620@163.com>
2026-09-05 10:56:25 +0800

    refactor(practice): 用自定义组件替换权限403提示
    - 用 ForbiddenState 组件替代原先的 NResult 组件显示无权限状态
    - 精简模版代码，提升可维护性
    - 保持无权限提示内容一致性

--- src/modules/practice/pages/InternshipManagementPage.vue
+++ src/modules/practice/pages/InternshipManagementPage.vue
@@ -20,10 +20,10 @@ import {
   NDatePicker,
   NRadioGroup,
   NRadio,
-  NResult,
   useMessage,
   type DataTableColumns,
 } from 'naive-ui'
+import ForbiddenState from '@/shared/components/ForbiddenState.vue'
 import EmptyState from '@/shared/components/EmptyState.vue'
 import {
   fetchInternships,
@@ -873,12 +873,7 @@ onMounted(() => {
 
 <template>
   <div class="practice-page">
-    <NResult
-      v-if="!canManage"
-      status="403"
-      :title="$t('practice.common.noPermission')"
-      :description="$t('practice.common.noPermissionDesc')"
-    />
+    <ForbiddenState v-if="!canManage" />
     <template v-else>
       <NTabs v-model:value="activeTab" type="line" animated @update:value="onTabChange">
         <!-- 实习项目 -->
